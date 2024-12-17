5a411d51b9cdcbce0e0ddc20107b431660eedc4a
kanak640 <kadam.kanak07@gmail.com>
2024-12-18 01:24:08 +0530

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the temporary arrays back into arr[left..right]
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << std::endl;
+}
+
+int main() {
+    int n,i;
+    cout<<"Enter number of students:"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter marks of students for subject data structure:";
+    for(i=0;i<n;i++)
+    {
+     cin>>arr[i];
+    }
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    mergeSort(arr, 0, size - 1);
+    
+    cout << "Students marks after sorting: ";
+    printArray(arr, size);
+    cout<<"Topper for the subject:"<<arr[n-1]<<endl;
+    return 0;
+}
+
